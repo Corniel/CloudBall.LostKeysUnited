54231a1d918b668c75e8a6586425344e6dfcbebd
Corniel <mail@corniel.nl>
2015-06-28 12:49:51 +0200

    Added perfromance tooling.

---
+++ src/CloudBall.Engines.LostKeysUnited.Performance/Program.cs
@@ -0,0 +1,13 @@
+﻿using CloudBall.Engines.LostKeysUnited.UnitTests;
+
+namespace CloudBall.Engines.LostKeysUnited.Performance
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var test = new BallPathTest();
+			test.Performance_Rnd();
+		}
+	}
+}
---
+++ src/CloudBall.Engines.LostKeysUnited.Performance/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CloudBall.Engines.LostKeysUnited.Performance")]
+[assembly: AssemblyDescription("Lost Keys United AI performance tests.")]
+[assembly: AssemblyCopyright("Copyright © Corniel Nobel, 2015")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

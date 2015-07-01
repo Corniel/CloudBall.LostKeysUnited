8fac42f7495de08317795b081b753a31785e23e4
Corniel <mail@corniel.nl>
2015-07-01 22:25:47 +0200

    Deleted some obslete stuff and added a missing file.

---
+++ src/CloudBall.Engines.LostKeysUnited.UnitTests/CloudBallAssert.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CloudBall.Engines.LostKeysUnited.UnitTests
+{
+	public static class CloudBallAssert
+	{
+		public static void AreEqual(Velocity expected, Velocity actual, float delta = 0.0001f)
+		{
+			var dX = Math.Abs(expected.X - actual.X);
+			var dY = Math.Abs(expected.Y - actual.Y);
+			if (dX > delta || dY > delta)
+			{
+				Assert.AreEqual(expected, actual);
+			}
+		}
+
+		public static void AreEqual(IPoint expected, IPoint actual, float delta = 0.0001f)
+		{
+			var dX = Math.Abs(expected.X - actual.X);
+			var dY = Math.Abs(expected.Y - actual.Y);
+			if (dX > delta || dY > delta)
+			{
+				Assert.AreEqual(expected, actual);
+			}
+		}
+	}
+}

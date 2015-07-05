260ca7b8d030dfd253c10e653850f7f8cc6ddf12
Corniel <mail@corniel.nl>
2015-07-05 22:25:48 +0200

    Tweeks for keeper role.

---
+++ src/CloudBall.Engines.LostKeysUnited.UnitTests/Roles/KeeperTest.cs
@@ -0,0 +1,50 @@
+﻿using CloudBall.Engines.LostKeysUnited.Roles;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudBall.Engines.LostKeysUnited.UnitTests.Roles
+{
+	[TestFixture]
+	public class KeeperTest
+	{
+		[Test]
+		public void GetTarget_AlmostTopCornerFlag_LocialPosition()
+		{
+			var act = Keeper.GetTarget(new Position(10, 200));
+			var exp = new Position(0.1f, 464.0f);
+
+			CloudBallAssert.AreEqual(exp, act, 0.1f);
+		}
+
+		[Test]
+		public void GetTarget_200x400y_LocialPosition()
+		{
+			var act = Keeper.GetTarget(new Position(200, 400));
+			var exp = new Position(16.3f, 487.6f);
+
+			CloudBallAssert.AreEqual(exp, act, 0.1f);
+		}
+
+		[Test]
+		public void GetTarget_800x539y_LocialPosition()
+		{
+			var act = Keeper.GetTarget(new Position(800, 539));
+			var exp = new Position(59.9f, 539.0f);
+
+			CloudBallAssert.AreEqual(exp, act, 0.1f);
+		}
+
+		[Test]
+		public void GetTarget_AlmostBottomCornerFlag_LocialPosition()
+		{
+			var act = Keeper.GetTarget(new Position(5, 900));
+			var exp = new Position(0.1f, 614.0f);
+
+			CloudBallAssert.AreEqual(exp, act, 0.1f);
+		}
+	}
+}
--- src/CloudBall.Engines.LostKeysUnited/Roles/Keeper.cs
+++ src/CloudBall.Engines.LostKeysUnited/Roles/Keeper.cs
@@ -7,20 +7,38 @@ namespace CloudBall.Engines.LostKeysUnited.Roles
 	/// <summary>The keeper is the last man standing, he will not leave his goal that easy.</summary>
 	public class Keeper : IRole
 	{
-		public const Single MaximumDistanceFromGoal = 210f;
+		public const Single MaximumDistanceFromGoal = 75f;
 
 		public bool Apply(GameState state, PlayerQueue queue)
 		{
-			var keeper = Goal.Own.GetClosestBy(queue);
-			var ball = state.Current.Ball;
+			if (queue.IsEmty) { return false; }
 
-			if (keeper != null)
+			var ball = state.Current.Ball.Position;
+
+			if (!state.Current.Ball.HasOwner && state.CatchUps.Count > 0)
 			{
-				var velo = (ball.Position - keeper.Position).Scale(MaximumDistanceFromGoal);
-				var target = Goal.Own.Center + velo;
-				queue.Dequeue(Actions.Move(keeper, target));
+				ball = state.CatchUps[0].Position;
 			}
-			return keeper != null;
+
+			var target = GetTarget(ball);
+
+			var keeper = target.GetClosestBy(queue);
+
+			queue.Dequeue(Actions.Move(keeper, target));
+			
+			return true;
+		}
+
+		public static Position GetTarget(Position ball)
+		{
+			var vTop = ball - Goal.Own.Top;
+			var vBot = ball - Goal.Own.Bottom;
+			var ang = Angle.Between(vTop, vBot);
+
+			var velo = vBot.Rotate((float)ang / 2f);
+			var move = velo.Scale(((float)vTop.Speed + (float)vBot.Speed) * 0.5 - MaximumDistanceFromGoal);
+			var target = ball - move;
+			return target;
 		}
 	}
 }
